7ff46284dfee823188cd42aba3b61d3a947d4cb4
Oleksii LEVENETS <oleksii.levenets@icloud.com>
2023-01-23 12:28:21 +0200

    after feedback

--- Task_1/script_1.py
+++ Task_1/script_1.py
@@ -3,6 +3,10 @@ Task_1.
 1. Напишіть ітератор який приймає додатнє число n і видає всі числа фібоначі від 0 до n
 (якщо число більше чим n, то воно не повертається). Добавте метод який перевіряє чи поточний стан ітератора
 – парне число.
+
+Добавте метод який перевіряє чи поточний стан ітератора --
+мається на увазі що в ітераторі буде метод, наприклад, is_odd() -> bool
+і ви тоді можете в любий момент робити fib.is_odd()
 """
 
 
@@ -17,10 +21,6 @@ class FibonacciNumbers:
 
     def __next__(self):
         if self.next > self.stop:
-            if self.current % 2 == 0:
-                print(f"{self.current} is even")
-            else:
-                print(f"{self.current} is odd")
             raise StopIteration
 
         next_num = self.current + self.next
@@ -28,7 +28,16 @@ class FibonacciNumbers:
         self.next = next_num
         return self.current
 
+    def is_even(self):
+        print("The current number of Iterator is even (True/False): ", end="")
+        if self.current % 2 == 0:
+            return True
+        else:
+            return False
 
-fib = FibonacciNumbers(40)
+
+fib = FibonacciNumbers(50)
 for i in fib:
     print(i)
+
+print(fib.is_even())
